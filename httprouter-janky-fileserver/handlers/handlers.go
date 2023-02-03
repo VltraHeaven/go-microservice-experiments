097f22d3ef8c995f858e8b305c8bca32e66f873c
Julio Hawthorne <julio@vltraheaven.io>
2023-02-02 23:30:04 -0800

    Added comments to httprouter-janky-fileserver/handlers/handlers.go

--- httprouter-janky-fileserver/handlers/handlers.go
+++ httprouter-janky-fileserver/handlers/handlers.go
@@ -7,17 +7,20 @@ import (
 	"os/exec"
 )
 
+// getCommandOutput is a wrapper function for the exec.Command function
 func getCommandOutput(cmd string, args ...string) string {
 	out, _ := exec.Command(cmd, args...).Output()
 	return string(out)
 }
 
+// GetGoVersion is a handler that returns the specified go binary's version
 func GetGoVersion(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	res := getCommandOutput("/usr/bin/go", "version")
 	io.WriteString(w, res)
 	return
 }
 
+// GetFileContent is a handler that receives a filename and returns the text contained within
 func GetFileContent(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	io.WriteString(w, getCommandOutput("/bin/cat", params.ByName("name")))
 	return
